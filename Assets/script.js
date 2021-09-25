c456869fb81c8d3fb8234d75ceb959d12e5a6c00
PFZM <pfzm@hotmail.com>
2021-09-25 18:31:51 +1000

    added fetch to API

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,9 @@
 const currentDate = document.querySelector("#date");
+const searchBtn = document.querySelector("#search-button");
+const cityInput = document.querySelector("#city");
+
+const APIKey = "fc1547c6c6eac0f4c70827baceb61b94";
+let city;
 
 // function to display current date
 function displayDate() {
@@ -6,3 +11,41 @@ function displayDate() {
   currentDate.textContent = date;
 }
 setInterval(displayDate, 1000);
+
+searchBtn.addEventListener("click", searchCity);
+
+function searchCity(event) {
+  event.preventDefault();
+
+  if (!cityInput.value) {
+    window.alert("Please enter a city name");
+    return;
+  }
+
+  city = cityInput.value;
+  console.log(city);
+
+  retrieveWeather(city);
+}
+
+function retrieveWeather(city) {
+  const queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+
+  fetch(queryURL)
+    .then(function (response) {
+      if (!response.ok) {
+        alert("Error: " + response.statusText + "\nPlease check the city name");
+      }
+      response.json().then(function (data) {
+        console.log(data);
+      });
+      console.log(response);
+    })
+    .catch(function (error) {
+      alert("Unable to retrieve data");
+    });
+}
